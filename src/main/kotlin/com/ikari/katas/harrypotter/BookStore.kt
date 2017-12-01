60fec0f718e2ef5622fe77b2f2e1f40fe33081bd
Juan Carlos García <juancarlos.garcia@colaboradores.renfe.es>
2017-12-01 12:22:18 +0100

    Working and all test in green for first set

--- src/main/kotlin/com/ikari/katas/harrypotter/BookStore.kt
+++ src/main/kotlin/com/ikari/katas/harrypotter/BookStore.kt
@@ -11,28 +11,113 @@ class BookStore(private val books: List<Book> = listOf()) {
 
     private fun calculateBasePrice() = books.size * 8.0
 
-    private fun calculateDiscount():Double {
+    private fun calculateDiscount(): Double {
         val hp1 = mutableListOf<Book>()
         val hp2 = mutableListOf<Book>()
         val hp3 = mutableListOf<Book>()
         val hp4 = mutableListOf<Book>()
         val hp5 = mutableListOf<Book>()
+        var discount = 0.0
 
-        for(b in books){
-            if(b is Book.HarryPotterI){
+        for (b in books) {
+            if (b is Book.HarryPotterI) {
                 hp1.add(b)
-            }
-            else if(b is Book.HarryPotterII){
+            } else if (b is Book.HarryPotterII) {
                 hp2.add(b)
-            }else if(b is Book.HarryPotterIII){
+            } else if (b is Book.HarryPotterIII) {
                 hp3.add(b)
-            }else if(b is Book.HarryPotterIV){
+            } else if (b is Book.HarryPotterIV) {
                 hp4.add(b)
-            }else if(b is Book.HarryPotterV){
+            } else if (b is Book.HarryPotterV) {
                 hp5.add(b)
             }
         }
-        return 0.0
+        if ((hp1.size == hp2.size) && (hp1.size == hp3.size) && (hp1.size == hp4.size) && (hp1.size == hp5.size))
+            discount = 0.25
+        else if (hp1.size != 0) {
+            if (hp2.size != 0) {
+                if (hp3.size != 0) {
+                    if (hp4.size != 0) {
+                        discount = 0.2
+                    } else if (hp5.size != 0) {
+                        discount = 0.2
+                    } else {
+                        discount = 0.1
+                    }
+                } else {
+                    if (hp4.size != 0) {
+                        if (hp5.size != 0) {
+                            discount = 0.2
+                        } else {
+                            discount = 0.1
+                        }
+                    } else if(hp5.size!=0) {
+                        discount = 0.1
+                    }else{
+                        discount = 0.05
+                    }
+                }
+            } else if (hp3.size != 0) {
+                if (hp4.size != 0) {
+                    if (hp5.size != 0) {
+                        discount = 0.2
+                    } else {
+                        discount = 0.1
+                    }
+                } else if (hp5.size != 0) {
+                    discount = 0.1
+                } else {
+                    discount = 0.05
+                }
+            } else if (hp4.size != 0) {
+                if(hp5.size!=0){
+                    discount = 0.1
+                }else {
+                    discount = 0.05
+                }
+            } else if (hp5.size != 0) {
+                discount = 0.05
+            }
+
+        } else if (hp2.size != 0) {
+            if (hp3.size != 0) {
+                if (hp4.size != 0) {
+                    if(hp5.size!=0){
+                        discount = 0.2
+                    }else {
+                        discount = 0.1
+                    }
+                } else if (hp5.size != 0) {
+                    discount = 0.1
+                } else {
+                    discount = 0.05
+                }
+            } else if (hp4.size != 0) {
+                if (hp5.size != 0) {
+                    discount = 0.1
+                } else {
+                    discount = 0.05
+                }
+            } else if (hp5.size != 0) {
+                discount = 0.05
+            }
+
+        } else if (hp3.size != 0) {
+            if (hp4.size != 0) {
+                if (hp5.size != 0) {
+                    discount = 0.1
+                } else {
+                    discount = 0.05
+                }
+            } else if (hp5.size != 0) {
+                discount = 0.05
+            }
+        } else if (hp4.size != 0) {
+            if (hp5.size != 0) {
+                discount = 0.05
+            }
+        }
+        return 1.0 - discount
     }
 
 }
--- src/test/spock/com/ikari/katas/harrypotter/BookStoreTest.groovy
+++ src/test/spock/com/ikari/katas/harrypotter/BookStoreTest.groovy
@@ -8,11 +8,11 @@ class BookStoreTest extends Specification {
 
     def "Sell a Book price is 8.0"() {
         given: 'A BookStore and a Harry Potter Book'
-            BookStore store = new BookStore()
+        BookStore store = new BookStore()
         when: 'add a book'
-            store = store.addBook(new Book.HarryPotterI())
+        store = store.addBook(new Book.HarryPotterI())
         then: 'price is 8.0'
-            store.price == 8.0
+        store.price == 8.0
     }
 
     def "Sell two equals Books price is 16.0"() {
@@ -26,18 +26,292 @@ class BookStoreTest extends Specification {
         store.price == 16.0
     }
 
-    def "Sell two diferent Books price is 15.2"() {
+    def "Sell Harry Potter I and II Books price is 15.2"() {
         given: 'A BookStore and a Harry Potter Book'
         BookStore store = new BookStore()
-        when: 'add a book'
+        when: 'add a Harry Potter I book'
         store = store.addBook(new Book.HarryPotterI())
-        and: 'add another different book '
+        and: 'add a Harry Potter II book'
         store = store.addBook(new Book.HarryPotterII())
         then: 'price is 15.2'
         store.price == 15.2
     }
 
-    def "Sell three diferent Books price is 21.6"() {
+    def "Sell Harry Potter I and III Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter III book '
+        store = store.addBook(new Book.HarryPotterIII())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter I and IV Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter IV book '
+        store = store.addBook(new Book.HarryPotterIV())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter I and V Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter V book '
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter II and III Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter III book '
+        store = store.addBook(new Book.HarryPotterIII())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter II and IV Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter IV book '
+        store = store.addBook(new Book.HarryPotterIV())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter II and V Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter V book '
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter III and IV Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter IV book '
+        store = store.addBook(new Book.HarryPotterIV())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter III and V Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter V book '
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter IV and V Books price is 15.2"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        and: 'add a Harry Potter V book '
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 15.2'
+        store.price == 15.2
+    }
+
+    def "Sell Harry Potter I, II and III Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter I, II and IV Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter I, II and V Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter I, III and IV Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter I, III and V Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter I, IV and V Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter II, III and IV Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter II, III and V Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter II, IV and V Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter III, IV and V Books price is 21.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 21.6'
+        store.price == 21.6
+    }
+
+    def "Sell Harry Potter I, II, III and IV Books price is 25.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        then: 'price is 25.6'
+        store.price == 25.6
+    }
+
+    def "Sell Harry Potter I, II, III and V Books price is 25.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when: 'add a Harry Potter I book'
+        store = store.addBook(new Book.HarryPotterI())
+        and: 'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 25.6'
+        store.price == 25.6
+    }
+
+    def "Sell Harry Potter II, III, IV and V Books price is 25.6"() {
+        given: 'A BookStore and a Harry Potter Book'
+        BookStore store = new BookStore()
+        when:'add a Harry Potter II book'
+        store = store.addBook(new Book.HarryPotterII())
+        and: 'add a Harry Potter III book'
+        store = store.addBook(new Book.HarryPotterIII())
+        and: 'add a Harry Potter IV book'
+        store = store.addBook(new Book.HarryPotterIV())
+        and: 'add a Harry Potter V book'
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 25.6'
+        store.price == 25.6
+    }
+
+    def "Sell Harry Potter I, II, III, IV and V Books price is 30"() {
         given: 'A BookStore and a Harry Potter Book'
         BookStore store = new BookStore()
         when: 'add a book'
@@ -46,8 +320,12 @@ class BookStoreTest extends Specification {
         store = store.addBook(new Book.HarryPotterII())
         and: 'add another different book '
         store = store.addBook(new Book.HarryPotterIII())
-        then: 'price is 21.6'
-        store.price == 21.6
+        and: 'add another different book '
+        store = store.addBook(new Book.HarryPotterIV())
+        and: 'add another different book '
+        store = store.addBook(new Book.HarryPotterV())
+        then: 'price is 30'
+        store.price == 30
     }
 
 }
